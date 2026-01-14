083b7f05b26a98a224e1f8a8d4e1349698667d22
stevejay675 <stevejay675@gmail.com>
2026-01-14 20:01:06 +0100

    fix build errors

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -69,7 +69,7 @@ const testimonials = [
   },
 ];
 
-const StarRating = ({ rating }) => {
+const StarRating = ({ rating }: { rating: number }) => {
   const fullStars = Math.floor(rating);
   const hasHalfStar = rating % 1 !== 0;
   const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
